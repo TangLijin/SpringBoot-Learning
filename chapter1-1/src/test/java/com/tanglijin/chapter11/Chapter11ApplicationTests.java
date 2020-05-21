320d122d62d005c7a1aa31b5efad52180f8314fc
LouisTang <louis.tang@cargosmart.com>
2020-05-21 15:20:52 +0800

    add HelloControllerand test

--- chapter1-1/src/test/java/com/tanglijin/chapter11/Chapter11ApplicationTests.java
+++ chapter1-1/src/test/java/com/tanglijin/chapter11/Chapter11ApplicationTests.java
@@ -1,13 +1,41 @@
 package com.tanglijin.chapter11;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 class Chapter11ApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
+//	@Before
+//	public void setUp(){
+//		mockMvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+//	}
+
 	@Test
-	void contextLoads() {
+	public void getHello() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("Hello World")));
 	}
 
 }
