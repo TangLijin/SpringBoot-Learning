7a45994812cc75e162cb1689e406972cf7d2334b
LouisTang <louis.tang@cargosmart.com>
2020-05-22 10:12:52 +0800

    refactor: 修改controller位置，变为典型目录结构

--- chapter1-1/src/main/java/com/tanglijin/chapter11/web/HelloController.java
+++ chapter1-1/src/main/java/com/tanglijin/chapter11/web/HelloController.java
@@ -1,4 +1,4 @@
-package com.tanglijin.chapter11;
+package com.tanglijin.chapter11.web;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
